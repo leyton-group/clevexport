ab9d755f126ff577f26dce26f58c1fae5f86b2a7
El Houssain INANI <einani@leyton.com>
2022-02-22 17:05:22 +0100

    Remove factory

--- src/Models/Export.php
+++ src/Models/Export.php
@@ -2,7 +2,6 @@
 
 namespace Leyton\ClevExport\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,8 +9,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Export extends Model
 {
-    use HasFactory;
-
     protected $guarded = [];
 
     protected $casts = [
--- src/Models/SubExport.php
+++ src/Models/SubExport.php
@@ -2,14 +2,11 @@
 
 namespace Leyton\ClevExport\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubExport extends Model
 {
-    use HasFactory;
-
     protected $guarded = [];
 
     protected $casts = [
